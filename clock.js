8054caa346e0d381e3581adba9a558ec61c0cd13
reetisharma105 <63422081+reetisharma105@users.noreply.github.com>
2020-07-14 20:50:57 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,16 @@
+const sechand = document.querySelector("#sec");
+const minHand = document.querySelector("#min");
+const hourHand = document.querySelector("#hour");
+function setDate() {
+  const now = new Date();
+  const sec = now.getSeconds();
+  const min = now.getMinutes();
+  const hour = now.getMinutes();
+  const secDeg = (sec / 60) * 360 + 90;
+  const minDeg = (min / 60) * 360 + 90;
+  const hourDeg = (min / 24) * 360 + 90;
+  sechand.style.transform = `rotate(${secDeg}deg)`;
+  minHand.style.transform = `rotate(${minDeg}deg)`;
+  hourHand.style.transform = `rotate(${hourDeg}deg)`;
+}
+setInterval(setDate, 1000);
