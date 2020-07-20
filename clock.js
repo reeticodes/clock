15774e0740d5d0f3e04350468c9b4ef3b31ce6e3
reetisharma105 <63422081+reetisharma105@users.noreply.github.com>
2020-07-20 09:24:45 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -5,10 +5,10 @@ function setDate() {
   const now = new Date();
   const sec = now.getSeconds();
   const min = now.getMinutes();
-  const hour = now.getMinutes();
+  const hour = now.getHours();
   const secDeg = (sec / 60) * 360 + 90;
   const minDeg = (min / 60) * 360 + 90;
-  const hourDeg = (min / 12) * 360 + 90;
+  const hourDeg = (hour / 12) * 360 + 90;
   sechand.style.transform = `rotate(${secDeg}deg)`;
   minHand.style.transform = `rotate(${minDeg}deg)`;
   hourHand.style.transform = `rotate(${hourDeg}deg)`;
